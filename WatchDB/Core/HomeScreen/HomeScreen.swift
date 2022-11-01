580740008c90a656d58895a5154f1ca4db7dd7e4
Oğuzhan Kertmen <iletisim@oguzhankertmen.com.tr>
2022-11-01 17:35:48 +0300

    Remove Storyboard
    Storyboard removed for programmatic UI

---
+++ WatchDB/Core/HomeScreen/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  WatchDB
+//
+//  Created by Oğuzhan KERTMEN on 1.11.2022.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      view.backgroundColor = .systemGray
+    }
+
+}
