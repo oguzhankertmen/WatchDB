8991c3125244cc8d3f946689813ebce71d97fd0c
Oğuzhan Kertmen <iletisim@oguzhankertmen.com.tr>
2022-11-04 02:03:02 +0300

    Add Movie model

---
+++ WatchDB/Models/Movie.swift
@@ -0,0 +1,22 @@
+//
+//  Movie.swift
+//  WatchDB
+//
+//  Created by Oğuzhan KERTMEN on 4.11.2022.
+//
+
+import Foundation
+
+struct MovieResult: Decodable {
+  let results: [Movie]?
+}
+
+struct Movie: Decodable {
+  let id: Int?
+  let posterPath: String?
+  
+  enum CodingKeys: String, CodingKey {
+    case id
+    case posterPath = "poster_path"
+  }
+}
