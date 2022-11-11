dc5d44432187af06064c771c665a8d2a7b0484f4
Oğuzhan Kertmen <iletisim@oguzhankertmen.com.tr>
2022-11-11 15:39:04 +0300

    Add an extension for figuring out device with and height

---
+++ WatchDB/Extensions/CGFloat+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  CGFloat+Extension.swift
+//  WatchDB
+//
+//  Created by Oğuzhan KERTMEN on 9.11.2022.
+//
+
+import UIKit
+
+extension CGFloat {
+  static let deviceWidth = UIScreen.main.bounds.width
+  static let deviceHeight = UIScreen.main.bounds.height
+}
