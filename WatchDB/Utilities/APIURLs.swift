d2981d5c15067bd9672ee55184d9c36f5b97fa1f
Oğuzhan Kertmen <iletisim@oguzhankertmen.com.tr>
2022-11-11 15:29:30 +0300

    Add Image URL function for creating image urls and download images

--- WatchDB/Utilities/APIURLs.swift
+++ WatchDB/Utilities/APIURLs.swift
@@ -11,4 +11,7 @@ enum APIURLs {
   static func movies(page: Int) -> String {
     "https://api.themoviedb.org/3/movie/popular?api_key=b9095663780407148e000e244ef8a85f&language=en-US&page=\(page)"
   }
+  static func imageURL(posterPath: String) -> String {
+    "https://image.tmdb.org/t/p/w500\(posterPath)"
+  }
 }
