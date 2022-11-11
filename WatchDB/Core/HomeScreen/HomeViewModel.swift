94bc17ec0adee9996ad22719a2f46c9689938965
Oğuzhan Kertmen <iletisim@oguzhankertmen.com.tr>
2022-11-11 15:40:44 +0300

    Add improvements for getting data from service layer

--- WatchDB/Core/HomeScreen/HomeViewModel.swift
+++ WatchDB/Core/HomeScreen/HomeViewModel.swift
@@ -11,14 +11,29 @@ protocol HomeViewModelInterface {
   var view: HomeScreenInterface? { get set }
   
   func viewDidLoad()
+  func getMovies()
 }
 
 final class HomeViewModel {
-   var view: HomeScreenInterface?
+  weak var view: HomeScreenInterface?
+  private let service = MovieService()
+  var movies: [Movie] = []
 }
 
 extension HomeViewModel: HomeViewModelInterface {
   func viewDidLoad() {
     view?.configureVC()
+    view?.configureCollectionView()
+    getMovies()
+  }
+  
+  func getMovies() {
+    service.downloadMovies { [weak self] returnedMovies in
+      guard let self = self else { return }
+      guard let returnedMovies = returnedMovies else {return}
+      
+      self.movies = returnedMovies
+      print(returnedMovies)
+    }
   }
 }
